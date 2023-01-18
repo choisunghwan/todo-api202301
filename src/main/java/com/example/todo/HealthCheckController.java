7253ee297db11f7e89d411a6e2b08870005cfd17
choisunghwan <dute7570@naver.com>
2023-01-18 14:18:36 +0900

    # 초기 설정 완료

---
+++ src/main/java/com/example/todo/HealthCheckController.java
@@ -0,0 +1,21 @@
+package com.example.todo;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*서버가 돌아가는지 확인해주기 위한 컨트롤러*/
+@RestController
+@Slf4j
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public ResponseEntity<?> check(){
+        log.info("server is running...");
+        return ResponseEntity
+                .ok()
+                .body("server is running...");
+    }
+}
