43bfbaa80d66574d81da21836037af06a7371227
choisunghwan <dute7570@naver.com>
2023-01-27 09:56:35 +0900

    # user api - 로그인검증 서비스 처리

--- src/main/java/com/example/todo/userapi/service/UserService.java
+++ src/main/java/com/example/todo/userapi/service/UserService.java
@@ -48,6 +48,27 @@ public class UserService {
         return userRepository.existsByEmail(email);
     }
 
+    // 로그인 검증
+    public UserEntity getByCredentials(
+            final String email,
+            final String rawPassword){
+        // 입력한 이메일을 통해 회원정보 조회
+        UserEntity originalUser = userRepository.findByEmail(email);
+
+        if(originalUser == null){
+            throw new RuntimeException("가입된 회원이 아닙니다.");
+        }
+        // 패스워드 검증 (입력 비번, DB에 저장된 비번)
+        if(!passwordEncoder.matches(rawPassword,originalUser.getPassword())){
+            throw new RuntimeException("비밀번호가 틀렸습니다.");
+        }
+
+        log.info("{}님 로그인 성공!", originalUser.getUserName());
+
+        return originalUser;
+
+    }
+
 
 }
 
--- src/test/java/com/example/todo/userapi/service/UserServiceTest.java
+++ src/test/java/com/example/todo/userapi/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.example.todo.userapi.service;
 
 import com.example.todo.userapi.dto.UserSignUpDTO;
 import com.example.todo.userapi.dto.UserSignUpResponseDTO;
+import com.example.todo.userapi.entity.UserEntity;
 import org.apache.catalina.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -50,6 +51,52 @@ class UserServiceTest {
         System.out.println("responseDTO = " + responseDTO);
         assertEquals("암호맨", responseDTO.getUserName());
 
+    }
+
+
+    @Test
+    @DisplayName("존재하지 않는 이메일로 로그인을 시도하면 Exception이 발생해야한다.")
+    void noUserTest() {
+        //given
+        String email = "dsfdsff@aasad.com";
+        String password = "1234";
+
+        //then
+        assertThrows(RuntimeException.class, () -> {
+
+            //when
+            userService.getByCredentials(email, password);
+        });
+    }
+
+
+    @Test
+    @DisplayName("틀린 비밀번호로 로그인을 시도하면 Exception이 발생해야한다..")
+    void invalidPasswordTest() {
+        //given
+        String email = "postman@aasad.com";
+        String password = "1234";
+
+        //then
+        assertThrows(RuntimeException.class, () -> {
+
+            //when
+            userService.getByCredentials(email, password);
+        });
+    }
+
+
+    @Test
+    @DisplayName("정확한 정보로 로그인을 시도하면 회원정보가 반환되어야 한다..")
+    void loginTest() {
+        //given
+        String email = "abcd@gmail.com";
+        String password = "1q2w3e4r!";
+
+        //when
+        UserEntity loginUser = userService.getByCredentials(email, password);
+        //then
+        assertEquals("야호", loginUser.getUserName());
     }
 
 }
